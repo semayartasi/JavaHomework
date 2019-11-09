a63fc813f84072893dd419b78413cc9d232fb0aa
Sema <semayartasi@hotmail.com>
2019-11-08 23:04:20 -0500

    HELLO JAVA

--- MyProject/src/day19_HashSet/HashSetTask11Retain.java
+++ MyProject/src/day19_HashSet/HashSetTask11Retain.java
@@ -1,26 +1,24 @@
-package day19.homework;
+package day19_HashSet;
 
-import java.util.ArrayList;
 import java.util.HashSet;
 
-public class HashSetTask10CompareTwoH {
+public class HashSetTask11Retain {
     public static void main(String[] args) {
         HashSet<String> colorsList1=new HashSet<String>();
         colorsList1.add("Black");
         colorsList1.add("Blue");
         colorsList1.add("Pink");
         colorsList1.add("Purple");
+        System.out.println("Hashset Content" +colorsList1);
 
         HashSet<String> colorsList2=new HashSet<String>();
         colorsList2.add("Black");
         colorsList2.add("Blue");
         colorsList2.add("Pink");
         colorsList2.add("Red");
-
-        HashSet<String>result=new HashSet<String>();
-        for (String s : colorsList1) {
-            System.out.println(colorsList2.contains(s)?"Yes":"No");
-        }
-
+        System.out.println("Hashset Content" +colorsList2);
+        colorsList1.retainAll(colorsList2);
+        System.out.println("hashset content:");
+        System.out.println(colorsList1);
     }
 }
--- MyProject/src/day19_HashSet/HashSetTask9ConvertArraySet.java
+++ MyProject/src/day19_HashSet/HashSetTask9ConvertArraySet.java
@@ -1,4 +1,4 @@
-package day19.homework;
+package day19_HashSet;
 
 import javax.sound.midi.Soundbank;
 import java.util.HashSet;
