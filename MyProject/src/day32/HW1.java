e6a5f93e071459742cd2f23f2df383c67c168323
Sema <semayartasi@hotmail.com>
2019-11-26 16:17:23 -0500

    HELLO JAVA

--- MyProject/src/day32/HW1.java
+++ MyProject/src/day32/HW1.java
@@ -7,17 +7,13 @@ public class HW1 {
     // ex: I go to store and bought a water
     // ex: I go to store and bought a sugar
     public static void main(String[] args) {
-    buyItem();
+        buyItem("milk");
+        buyItem("water");
+        buyItem("sugar");
     }
 
-    public static void buyItem() {
-        boolean dontHaveMilk = false;
-        if (dontHaveMilk) {
-            System.out.println("I'll to go to other store");
-            return;
-        }
-        System.out.println("buy milk");
-        System.out.println("buy bread");
-        System.out.println("buy water");
+    static void buyItem( String items ){
+        System.out.println( "I go to store and bought a " +items);
+
     }
 }
